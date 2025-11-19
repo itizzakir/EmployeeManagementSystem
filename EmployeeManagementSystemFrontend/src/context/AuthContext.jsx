95f1f7e682fd01e600542df7b7f705b1b25f4e26
Md Zakir Hussain <itizzakir@gmail.com>
2025-11-19 17:07:49 +0530

    Navigate Error Fixed

--- EmployeeManagementSystemFrontend/src/context/AuthContext.jsx
+++ EmployeeManagementSystemFrontend/src/context/AuthContext.jsx
@@ -66,6 +66,7 @@ export const AuthProvider = ({ children }) => {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   return useContext(AuthContext);
 };
